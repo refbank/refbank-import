6fbdce3bf6e4fdbc2e2e263bead238ccfe66c463
Ryder Fried <rmfnext@gmail.com>
2025-07-02 14:57:22 -0700

    added native_speaker column to the big dataset

--- import/hawkins_dynamics_cued/import.R
+++ import/hawkins_dynamics_cued/import.R
@@ -137,7 +137,7 @@ options=c("A", "B","C","D","E","F","G","H","I", "J", "K", "L")
 
 
 all <- choices |> bind_rows(messages) |> bind_rows(no_describer) |> bind_rows(choice_no_describer) |> 
-  left_join(exclude) |> 
+  left_join(exclude) |> left_join(select(sequentialSubjInfo, "gameid", "role", "nativeEnglish")) |>
   mutate(dataset_id="hawkins2020_characterizing_cued",
          full_cite="Hawkins, R. D., Frank, M. C., & Goodman, N. D. (2020). Characterizing the dynamics of learning in repeated reference games. Cognitive science, 44(6), e12845.",
          short_cite="Hawkins et al. (2020)",
@@ -148,6 +148,7 @@ all <- choices |> bind_rows(messages) |> bind_rows(no_describer) |> bind_rows(ch
          room_num=1,
          age=as.numeric(NA), #we may have this?
          gender=as.character(NA),
+         native_speaker=(nativeEnglish==tolower("yes")),
          trial_num=trialNum, 
          rep_num=repetitionNum,
          role=case_when(
@@ -168,7 +169,7 @@ all <- choices |> bind_rows(messages) |> bind_rows(no_describer) |> bind_rows(ch
   select(dataset_id, condition_label, full_cite, short_cite,
          stage_num, room_num, 
          language,
-         game_id, player_id, age, gender, trial_num,rep_num,
+         game_id, player_id, age, gender, native_speaker, trial_num,rep_num,
          role, target, message_number, text,
          choice_id, time_stamp, option_set,
          group_size,
