afce4ad5008447b32eb4ac8d363facc9ab875dc7
vboyce <vero.boyce@gmail.com>
2025-11-26 16:13:58 -0800

    update kid tangrams with more and cleaner data

--- import/boyce2025_preschoolers/import.R
+++ import/boyce2025_preschoolers/import.R
@@ -2,38 +2,152 @@ library(tidyverse)
 library(here)
 source(here("validate.R"))
 
-demogs <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/clean_demog_1_2.csv"
 
-expt_1_transcript <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_1/timed_transcript.csv"
+expt_1_transcript <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_1/transcripts.csv"
 expt_1_responses <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_1/clean_data.csv"
+expt_1_demog <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_1/clean_demog.csv"
+expt_1_link <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_1/link_transcripts.csv"
+
 
 expt_2_transcript <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_2/transcripts.csv"
 expt_2_responses <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_2/clean_data.csv"
+expt_2_link <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_2/link_transcripts.csv"
+expt_2_demog <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_2/clean_demog.csv"
 
-expt_1_messages <- read_csv(url(expt_1_transcript)) |>
-  filter(role %in% c("S", "L")) |>
-  select(game, text, description, speaker, trial, role) |>
-  group_by(game, trial) |>
+
+expt_3_transcript <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_3/transcripts.csv"
+expt_3_responses <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_3/clean_data.csv"
+expt_3_link <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_3/link_transcripts.csv"
+expt_3_demog <- "https://raw.githubusercontent.com/vboyce/kid-tangrams/refs/heads/main/data/expt_3/clean_demog.csv"
+
+
+
+expt_1_data <- read_csv(url(expt_1_responses)) |>
+  rename(trial = trialNum) |>
+  mutate(type = case_when(
+    trial < 2 ~ "practice",
+    trial < 6 ~ "block 1",
+    trial < 10 ~ "block 2",
+    trial < 14 ~ "block 3",
+  )) |>
+  filter(!is.na(response)) |>
+  mutate(correct = as.numeric(correct)) |>
+  mutate(expt = "1") |>
+  mutate(trial = trial - 2)
+
+expt_1_transcript <- read_csv(url(expt_1_transcript)) |>
+  filter(on_topic == "Y") |>
+  mutate(type = case_when(
+    trial < 2 ~ "practice",
+    trial < 6 ~ "block 1",
+    trial < 10 ~ "block 2",
+    trial < 14 ~ "block 3",
+  )) |>
+  mutate(expt = "1") |>
+  mutate(trial = trial - 2)
+
+expt_2_data <- read_csv(url(expt_2_responses)) |>
+  rename(trial = trialNum) |>
+  mutate(type = case_when(
+    trial < 4 ~ "practice",
+    trial < 8 ~ "block 1",
+    trial < 12 ~ "block 2",
+    trial < 16 ~ "block 3",
+    trial < 20 ~ "block 4"
+  )) |>
+  filter(!is.na(response)) |>
+  mutate(correct = as.numeric(correct)) |>
+  mutate(expt = "2") |>
+  mutate(trial = trial - 4)
+
+expt_2_transcript <- read_csv(url(expt_2_transcript)) |>
+  filter(on_topic == "Y") |>
+  mutate(type = case_when(
+    trial < 4 ~ "practice",
+    trial < 8 ~ "block 1",
+    trial < 12 ~ "block 2",
+    trial < 16 ~ "block 3",
+    trial < 20 ~ "block 4"
+  )) |>
+  mutate(expt = "2") |>
+  mutate(trial = trial - 4)
+
+
+
+expt_3_data <- read_csv(url(expt_3_responses)) |>
+  rename(trial = trialNum) |>
+  mutate(type = case_when(
+    trial < 4 ~ "practice",
+    trial < 8 ~ "block 1",
+    trial < 12 ~ "block 2",
+    trial < 16 ~ "block 3",
+    trial < 20 ~ "block 4"
+  )) |>
+  filter(!is.na(response)) |>
+  mutate(correct = as.numeric(correct)) |>
+  mutate(expt = "3") |>
+  mutate(trial = trial - 4)
+
+expt_3_transcript <- read_csv(url(expt_3_transcript)) |>
+  filter(on_topic == "Y") |>
+  mutate(type = case_when(
+    trial < 4 ~ "practice",
+    trial < 8 ~ "block 1",
+    trial < 12 ~ "block 2",
+    trial < 16 ~ "block 3",
+    trial < 20 ~ "block 4"
+  )) |>
+  mutate(expt = "3") |>
+  mutate(trial = trial - 4)
+
+
+demogs <- read_csv(url(expt_1_demog)) |>
+  mutate(expt = as.character(expt)) |>
+  bind_rows(read_csv(url(expt_2_demog)) |> mutate(expt = as.character(expt))) |>
+  bind_rows(read_csv(url(expt_3_demog)) |>
+    mutate(expt = as.character(expt))) |>
+  mutate(
+    age = age_month / 12,
+    hispanic = case_when(
+      hispanic == "yes" ~ "hispanic",
+      T ~ ""
+    ),
+    race = str_c(ethnicity, " ", hispanic)
+  )
+
+# construct apprpriate chunks
+
+messages <- expt_1_transcript |>
+  bind_rows(expt_2_transcript) |>
+  bind_rows(expt_3_transcript) |>
+  select(game, text, speaker, trial, role, type, expt) |>
+  group_by(game, trial, expt) |>
   mutate(message_number = row_number()) |>
   ungroup() |>
   mutate(
-    trialNum = trial - 1,
     action_type = "message",
-    message_irrelevant = ifelse(is.na(description), T, F),
-    role = ifelse(role == "S", "describer", "matcher")
+    role = ifelse(role == "S", "describer", "matcher"),
+    message_irrelevant = NA
   ) |>
-  filter(trialNum > 0) |>
+  filter(type != "practice") |>
   select(game,
-    playerId = speaker, role, trialNum, action_type,
-    message_number, text, message_irrelevant
+    playerId = speaker, role, trial, action_type,
+    message_number, text, message_irrelevant, expt
   )
 
-expt_1_choices <- read_csv(url(expt_1_responses)) |>
-  filter(!is.na(repNum)) |>
-  select(game, trialNum, response, listener, distractor, target) |>
-  mutate(
-    trialNum = trialNum - 1,
-  ) |>
+choices <- expt_1_data |>
+  bind_rows(expt_2_data) |>
+  bind_rows(expt_3_data) |>
+  inner_join(read_csv(url(expt_1_link)) |>
+    mutate(expt = "1") |>
+    bind_rows(
+      read_csv(url(expt_2_link)) |> mutate(expt = "2")
+    ) |>
+    bind_rows(
+      read_tsv(url(expt_3_link)) |> mutate(expt = "3")
+    ) |> select(game, expt)) |>
+  filter(type != "practice") |>
+  select(game, trial, response, listener, distractor, target, expt) |>
   rename(playerId = listener, choice_id = response) |>
   mutate(choice_id = ifelse(is.na(choice_id), "timed_out", choice_id)) |>
   mutate(
@@ -41,190 +155,55 @@ expt_1_choices <- read_csv(url(expt_1_responses)) |>
     role = "matcher"
   )
 
-expt_1_no_talk <- read_csv(url(expt_1_responses)) |>
-  filter(!is.na(repNum)) |>
-  select(game, trialNum, response, speaker) |>
-  mutate(
-    trialNum = trialNum - 1,
+no_talk <- expt_1_data |>
+  bind_rows(expt_2_data) |>
+  bind_rows(expt_3_data) |>
+  filter(type != "practice") |>
+  select(expt, game, trial, response, playerId = speaker) |>
+  anti_join(messages) |>
+  mutate(text = NA, role = "describer", action_type = "message")
+
+
+exclude <- read_csv(url(expt_1_link)) |>
+  mutate(expt = "1") |>
+  bind_rows(
+    read_csv(url(expt_2_link)) |> mutate(expt = "2")
   ) |>
-  anti_join(expt_1_messages) |>
-  mutate(text = NA, playerId = speaker, role = "describer", action_type = "message")
+  bind_rows(
+    read_tsv(url(expt_3_link)) |> mutate(expt = "3")
+  ) |>
+  filter(!is.na(exclude)) |>
+  mutate(exclude = T)
 
-
-expt_1_exclude <- expt_1_no_talk |>
-  select(game, trialNum) |>
+no_talk_exclude <- no_talk |>
+  select(game, trial, expt) |>
+  anti_join(exclude |> select(game, expt)) |>
   mutate(exclude = T, exclusion_reason = "no describer talk")
 
-expt_1_options <- expt_1_choices |>
-  select(target, distractor, trialNum, game) |>
-  mutate(option_set = str_c(target, distractor, sep = ";")) |>
-  select(-distractor)
-
-expt_1 <- expt_1_messages |>
-  bind_rows(expt_1_choices) |>
-  bind_rows(expt_1_no_talk) |>
-  mutate(condition_label = "expt_1") |>
-  select(-target) |>
-  left_join(expt_1_options) |>
-  left_join(expt_1_exclude)
-
-
-id_sub <- tribble(
-  ~playerId, ~actual,
-  "apple123", "id74",
-  "pear124", "id75",
-  "apple127", "id76",
-  "pear128", "id77",
-  "80", "id78",
-  "81", "id79",
-  "apple111", "id90",
-  "pear222", "id91",
-  "apple333", "id92",
-  "pear444", "id93",
-  "apple255", "id94",
-  "pear257", "id95"
-)
-
-expt_2_messages <- read_csv(url(expt_2_transcript)) |>
-  filter(speaker != "R") |> # note that this also indicates transcript error!
-  filter(role %in% c("S", "L")) |>
-  select(game = gameConfig, gameId, description, text, speaker, trial, role) |>
-  group_by(game, gameId, trial) |>
-  mutate(message_number = row_number()) |>
-  ungroup() |>
-  mutate(
-    trialNum = trial - 3,
-    action_type = "message",
-    message_irrelevant = ifelse(is.na(description), T, F),
-    role = ifelse(role == "S", "describer", "matcher")
-  ) |>
-  filter(trialNum > 0) |>
-  select(game, gameId,
-    playerId = speaker, role, trialNum, action_type,
-    message_number, text, message_irrelevant
-  ) |>
-  left_join(id_sub) |>
-  mutate(actual = case_when(
-    !is.na(actual) ~ actual,
-    str_detect(playerId, "id") ~ playerId,
-    T ~ str_c("id", playerId)
-  )) |>
-  select(-playerId) |>
-  rename(playerId = actual)
-
-expt_2_game_mapping <- expt_2_messages |>
-  select(game, gameId) |>
-  unique()
-
-expt_2_choices <- read_csv(url(expt_2_responses)) |>
-  filter(!is.na(repNum)) |>
-  select(gameId, trialNum, response, listener, target, distractor) |>
-  mutate(
-    trialNum = trialNum - 3,
-  ) |>
-  rename(playerId = listener, choice_id = response) |>
-  mutate(action_type = "selection", role = "matcher") |>
-  mutate(choice_id = ifelse(is.na(choice_id), "timed_out", choice_id)) |>
-  left_join(expt_2_game_mapping) |>
-  left_join(id_sub) |>
-  mutate(actual = case_when(
-    !is.na(actual) ~ actual,
-    str_detect(playerId, "id") ~ playerId,
-    T ~ str_c("id", playerId)
-  )) |>
-  select(-playerId) |>
-  rename(playerId = actual)
-
-expt_2_no_talk <- read_csv(url(expt_2_responses)) |>
-  filter(!is.na(repNum)) |>
-  select(gameId, trialNum, response, speaker) |>
-  left_join(expt_2_game_mapping) |>
-  mutate(
-    trialNum = trialNum - 3,
-  ) |>
-  anti_join(expt_2_messages) |>
-  mutate(text = NA, playerId = speaker, role = "describer", action_type = "message")
-
-echoing <- read_csv(url(expt_2_transcript)) |>
+echo_exclude <- expt_2_transcript |>
+  bind_rows(expt_3_transcript) |>
   filter(!is.na(echo)) |>
-  mutate(
-    trialNum = trial - 3,
-  ) |>
-  filter(trialNum > 0) |>
-  select(game = gameConfig, trialNum) |>
-  mutate(exclude = T, exclusion_reason = "researcher echoing")
+  select(game, trial, expt) |>
+  anti_join(exclude |> select(game, expt)) |>
+  anti_join(no_talk_exclude |> select(game, trial, expt)) |>
+  mutate(exclude = T, exclusion_reason = "experimentor echoed description")
 
-expt_2_exclude <- expt_2_no_talk |>
-  select(game, trialNum) |>
-  mutate(exclude = T, exclusion_reason = "no describer talk") |>
-  bind_rows(echoing)
+exclude_all <- exclude |> bind_rows(no_talk_exclude, echo_exclude)
 
-expt_2_options <- expt_2_choices |>
-  select(target, distractor, trialNum, game) |>
+options <- choices |>
+  select(target, distractor, trial, game, expt) |>
   mutate(option_set = str_c(target, distractor, sep = ";")) |>
   select(-distractor)
 
-expt_2 <- expt_2_messages |>
-  bind_rows(expt_2_choices) |>
-  bind_rows(expt_2_no_talk) |>
-  mutate(condition_label = "expt_2") |>
-  select(-gameId) |>
+all <- messages |>
+  bind_rows(choices) |>
+  bind_rows(no_talk) |>
   select(-target) |>
-  left_join(expt_2_options) |>
-  left_join(expt_2_exclude) |>
-  mutate(playerId = case_when(
-    game == "game76" & playerId == "id130" ~ "id139",
-    T ~ playerId
-  )) # fix a typo
-
-
-demographics <- read_csv((url(demogs))) |>
-  mutate(
-    age = age_month / 12,
-    hispanic = ifelse(hispanic == "yes", "hispanic", ""),
-    race = str_c(hispanic, ethnicity),
-    education = as.character(NA), # we are not making a "preschool" category
-    player_id = id
-  )
-
-
-all <- expt_1 |>
-  bind_rows(expt_2) |>
-  rename(game_id = game, player_id = playerId, trial_num = trialNum)
-
-
-all_fix_role <- all |>
-  select(condition_label, game_id, player_id, trial_num, role) |>
-  mutate(trial_parity = trial_num %% 2 == 0) |>
-  group_by(trial_parity, condition_label, game_id, player_id, role) |>
-  tally() |>
-  pivot_wider(values_from = n, names_from = role, values_fill = 0) |>
-  mutate(role = ifelse(describer > matcher, "describer", "matcher")) |>
-  select(trial_parity, condition_label, game_id, player_id, role)
-
-
-
-all_fixed <- all |>
-  mutate(trial_parity = trial_num %% 2 == 0) |>
-  select(-role) |>
-  left_join(all_fix_role) |>
-  select(-trial_parity)
-
-
-missing_describers <- all_fixed |>
-  select(game_id, trial_num, condition_label, target, option_set) |>
-  unique() |>
-  left_join(all_fixed |> filter(role == "describer")) |>
-  filter(is.na(role)) |>
-  mutate(role = "describer") |>
-  mutate(trial_parity = trial_num %% 2 == 0) |>
-  select(trial_parity, trial_num, condition_label, game_id, role, target, option_set) |>
-  left_join(all_fix_role) |>
-  mutate(action_type = "message")
-
-fixed <- all_fixed |>
-  bind_rows(missing_describers) |>
-  left_join(demographics) |>
+  left_join(exclude_all) |>
+  left_join(options) |>
+  left_join(demogs |> rename(playerId = id), by = c("expt", "game", "playerId")) |>
+  filter(!is.na(target)) |>
+  rename(condition_label = expt) |>
   mutate(
     dataset_id = "boyce2025_preschoolers",
     full_cite = "Boyce, V., Sparks, R., Mofor, Y., & Frank, M. C. Preschoolers can form conventional pacts with each other to communicate about novel referents. Preprint. 2025.",
@@ -242,12 +221,42 @@ fixed <- all_fixed |>
     order_match = "match",
     room_num = 1,
     stage_num = 1,
-    rep_num = (trial_num - 1) %/% 4 + 1,
+    rep_num = (trial) %/% 4 + 1,
+    trial_num = trial + 1,
     time_stamp = as.numeric(NA),
     native_language = as.character(NA),
+    education = "preschool",
     message_number = as.numeric(message_number),
-    game_id = str_c(condition_label, "_", game_id),
-  ) |>
+    game_id = str_c(condition_label, "_", game),
+    player_id = str_c(condition_label, "_", playerId),
+  ) |> # note that there are some limited cases where a participant is repeated
+  mutate(player_id = case_when(
+    # duplicate within experiment
+    player_id == "expt3_id186" ~ "expt2_id67",
+    player_id == "expt3_id185" ~ "expt3_id182",
+    player_id == "expt3_id210" ~ "expt3_id192",
+    player_id == "expt3_id269" ~ "expt3_id183",
+    player_id == "expt3_id213" ~ "expt3_id159",
+    player_id == "expt3_id214" ~ "expt2_id95",
+
+    # across expt 2 and 3 was allowed
+    player_id == "expt3_id204" ~ "expt2_id61",
+    player_id == "expt3_id181" ~ "expt2_id67",
+    player_id == "expt3_id252" ~ "expt2_id71",
+    player_id == "expt3_id159" ~ "expt2_id92",
+    player_id == "expt3_id160" ~ "expt2_id95",
+    player_id == "expt3_id176" ~ "expt2_id98",
+    player_id == "expt3_id157" ~ "expt2_id140",
+    player_id == "expt3_id238" ~ "expt2_id148",
+
+    # across expt 1 and 2 was not intended
+    player_id == "expt2_id113" ~ "expt1_id5",
+    player_id == "expt2_id112" ~ "expt1_id10",
+    player_id == "expt2_id110" ~ "expt1_id36",
+    player_id == "expt2_id129" ~ "expt1_id55",
+    player_id == "expt2_id133" ~ "expt1_id57",
+    T ~ player_id,
+  )) |>
   select(
     dataset_id, full_cite, short_cite, group_size, language, condition_label,
     prior_relationship, partner_constancy, population, role_constancy,
@@ -261,10 +270,4 @@ fixed <- all_fixed |>
     text, message_number, message_irrelevant, choice_id
   )
 
-# we think there are rare transcription errors in role, but we know that role alternated by trialnum
-# so until V fixes in original, do with majority fix
-
-
-
-
-validate_dataset(fixed, write = T)
+validate_dataset(all, write = T)
